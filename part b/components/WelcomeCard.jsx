b74ade6564c5af181e2c05e0141570f633c619f5
raffybonilla <126968781+raffybonilla@users.noreply.github.com>
2025-06-09 00:22:41 +0800

    Delete part b directory

--- part b/components/WelcomeCard.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './WelcomeCard.module.css';
-
-export default function WelcomeCard({ name }) {
-  return (
-    <div className={styles.card}>
-      <h2>Welcome, {name}!</h2>
-    </div>
-  );
-}
